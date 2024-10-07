41930e4bf53f99dee9d6fa2ecd0dd5867581f875
PREETHI3312 <151625222+PREETHI3312@users.noreply.github.com>
2024-10-07 11:07:44 +0530

    Update SRFLIPFLOPUSINGCASE.v

--- SRFLIPFLOPUSINGCASE.v
+++ SRFLIPFLOPUSINGCASE.v
@@ -4,14 +4,15 @@ module SRFLIPFLOPUSINGCASE(q, q_bar, s,r, clk, reset);//SR Flip Flop Behavioral 
   output q_bar;
  
   always@(posedge clk) begin // for synchronous reset
-    if(!reset)       q <= 0;
+    if(!reset)       
+	    q <= 0;
     else 
   begin
       case({s,r})       
 	     2'b00: q <= q;     // No change
-                               // Write logic for reset
-                              // Write logic for set
-                             // Write logic for Invalid state
+             2'b01: q<=1'b0;// Write logic for reset
+             2'b10: q<=1'b1;                  // Write logic for set
+             2'b11: q<=1'bx;               // Write logic for Invalid state
       endcase
     end
   end
